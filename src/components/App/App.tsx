b73eb5b04905553054cc168af1e1e55155b5b55b
iboyperson <tjquillan@gmail.com>
2020-03-25 23:45:05 -0700

    Convert App to functional component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import logo from '../../logo.svg'
 import { StateUpdater, ParsedState } from '../../helpers/ConfigParser/ConfigUpdater';
-import { Elements, Teams, FilterDisplay, Matches } from '../../helpers/ConfigParser/types';
 import { TeamList } from '../TeamList';
 import PacmanLoader from 'react-spinners/PacmanLoader';
 import { PrimaryContext, MatchesContext } from '../contexts';
@@ -11,75 +10,60 @@ import { TeamOverview } from '../TeamOverview/TeamOverview';
 import './App.css';
 
 
-interface AppState {
-  matches: Matches;
-  elements: Elements;
-  teams: Teams;
-  filters: Array<FilterDisplay>;
-  loading: boolean;
-}
-
-export class App extends React.Component<{}, Readonly<AppState>> {
-  readonly state: Readonly<AppState> = {
-    matches: {},
-    elements: {},
-    teams: [],
-    filters: [],
-    loading: true
-  }
-
-  private updater: Readonly<StateUpdater> = new StateUpdater((data: ParsedState) => this.setState(data), () => this.setState({loading: false}));
-
-  constructor(props: Readonly<{}>) {
-    super(props)
-
-    this.updater.start()
-  }
-
-  renderBody() {
-    if (this.state.loading) {
-      return (
-        <div className="Loading">
-          <PacmanLoader color="#6aa84f" loading={this.state.loading} />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <BrowserRouter>
-            <Switch>
-              <PrimaryContext.Provider value={{elements: this.state.elements, teams: this.state.teams}}>
-                <Route path="/" render={() => <TeamList filters={this.state.filters}/>} exact />
-                <Route path="/team/:teamNum" component={TeamOverview} />
-                <MatchesContext.Provider value={this.state.matches}>
-                  <Route path="/match/:matchKey" component={MatchPreview} />
-                </MatchesContext.Provider>
-              </PrimaryContext.Provider>
-            </Switch>
-          </BrowserRouter>
-        </div>
-      )
-    }
-  }
-
-  render() {
+function renderBody(state: ParsedState, loading: boolean) {
+  if (loading) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">TeamTracker</h1>
-          <p>
-            Team Scouting App created by the AZTECH
-          </p>
-        </header>
-        {this.renderBody()}
-        <footer className="App-footer">
-          <p>
-            Made by{' '}
-            <a href="https://team6479.org">AZTECH 6479</a>
-          </p>
-        </footer>
+      <div className="Loading">
+        <PacmanLoader color="#6aa84f" loading={loading} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <PrimaryContext.Provider value={{elements: state.elements, teams: state.teams}}>
+              <Route path="/" render={() => <TeamList filters={state.filters}/>} exact />
+              <Route path="/team/:teamNum" component={TeamOverview} />
+              <MatchesContext.Provider value={state.matches}>
+                <Route path="/match/:matchKey" component={MatchPreview} />
+              </MatchesContext.Provider>
+            </PrimaryContext.Provider>
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
+}
+
+export const App = () => {
+  const [state, setState] = useState<ParsedState>({
+    matches: {},
+    elements: {},
+    teams: [],
+    filters: []
+  })
+  const [loading, setLoading] = useState(true)
+  const [updater,] = useState<StateUpdater>(new StateUpdater((data: ParsedState) => setState(data), () => setLoading(false)))
+
+  useEffect(() => updater.start(), [updater])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">TeamTracker</h1>
+        <p>
+          Team Scouting App created by the AZTECH
+        </p>
+      </header>
+      {renderBody(state, loading)}
+      <footer className="App-footer">
+        <p>
+          Made by{' '}
+          <a href="https://team6479.org">AZTECH 6479</a>
+        </p>
+      </footer>
+    </div>
+  )
 }
